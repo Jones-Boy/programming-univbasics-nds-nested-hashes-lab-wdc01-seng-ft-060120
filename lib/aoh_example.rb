83cd6393c06492f7ace49fa13705a08e8cca1ab2
Michael Jones <djyoungjones@gmail.com>
2020-05-25 23:49:05 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -6,9 +6,8 @@ PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" }
 KATE_G = { name:  "Kate Gompert", occupation: "None" }
 BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
-def assembled_aoh
-  # Build an array that contains (or, "nests") the constants into a single
-  # Array. Ruby constants are denoted by ALL_CAPS
+def assembled_aoh( first, nests)
+ 
 end
 
 def literal_aoh
@@ -23,3 +22,4 @@ def aoh_update(aoh, row, key, new_value)
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
 end
+
